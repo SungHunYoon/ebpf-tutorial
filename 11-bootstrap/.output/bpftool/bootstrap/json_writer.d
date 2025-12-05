6abd5be2f06174435c7479351d399caecd15697c
Sunghun Yoon <gnsals6004@gmail.com>
2025-12-05 06:12:02 +0000

    update: gitignore

--- 11-bootstrap/.output/bpftool/bootstrap/json_writer.d
+++
@@ -1,5 +0,0 @@
-/home/sungyoon/ebpf-tutorial/11-bootstrap/.output/bpftool/bootstrap/json_writer.o: \
- json_writer.c json_writer.h \
- /home/sungyoon/ebpf-tutorial/third_party/bpftool/include/linux/compiler.h \
- /home/sungyoon/ebpf-tutorial/third_party/bpftool/include/linux/compiler_types.h \
- /home/sungyoon/ebpf-tutorial/third_party/bpftool/include/linux/compiler-gcc.h
